48c39110d8446cb51b5bcbb2443e48df478846ee
hamed <naeemaei@outlook.com>
2021-11-04 16:00:35 +0330

    Create new action

--- BarsaTutorial.Web/Controllers/AdminController.cs
+++ BarsaTutorial.Web/Controllers/AdminController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BarsaTutorial.Web.Data;
+using BarsaTutorial.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BarsaTutorial.Web.Controllers
 {
     public class AdminController : Controller
     {
+        public AdminController(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        public ApplicationDbContext ApplicationDbContext { get; }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> GetEducations(GetEducationsRequest request)
+        {
+            var query = ApplicationDbContext.Educations
+                .Include(e => e.Category)
+                .Include(e => e.FileType)
+                .Include(e => e.Lessons).AsQueryable();
+
+            if (request.CategoryID.HasValue)
+                query = query.Where(e => e.CategoryID == request.CategoryID.Value);
+
+            if (request.FileTypeID.HasValue)
+                query = query.Where(e => e.FileTypeID == request.FileTypeID.Value);
+
+            if (request.ID.HasValue)
+                query = query.Where(e => e.ID == request.ID.Value);
+
+            if (string.IsNullOrEmpty(request.Title))
+                query = query.Where(e => e.Title.Contains(request.Title));
+
+            var educations = await query.ToListAsync();
+
+            var responseList = new List<GetEducationsResponse>();
+
+            foreach (var item in educations)
+            {
+                var edu = new GetEducationsResponse
+                {
+                    ID = item.ID,
+                    CategoryID = item.CategoryID,
+                    CategoryTitle = item.Category.Title,
+                    Title = item.Title,
+                    FileTypeID = item.FileTypeID,
+                    FileTypeTitle = item.FileType.Title,
+                    LessonCount = item.Lessons.Count()
+                };
+
+                responseList.Add(edu);
+            }
+            return View(responseList);
+        }
+
     }
 }
---
+++ BarsaTutorial.Web/Models/ViewModels/GetEducations.cs
@@ -0,0 +1,21 @@
+﻿namespace BarsaTutorial.Web.Models.ViewModels
+{
+    public class GetEducationsRequest
+    {
+        public int? ID { get; set; }
+        public string Title { get; set; }
+        public int? CategoryID { get; set; }
+        public int? FileTypeID { get; set; }
+    }
+
+    public class GetEducationsResponse
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int CategoryID { get; set; }
+        public int FileTypeID { get; set; }
+        public string CategoryTitle { get; set; }
+        public string FileTypeTitle { get; set; }
+        public int LessonCount { get; set; }
+    }
+}
